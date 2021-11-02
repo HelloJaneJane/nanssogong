8b5edaaf82f938cb3b9cebaef29c1b84fa281bb2
이제인 <janeyi@naver.com>
2021-11-02 19:37:00 +0900

    mvm init

---
+++ MVM/MVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVM
+//
+//  Created by 이제인 on 2021/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
